cad05e816f477683f40e67327434124f3fe09627
dearhakeemdavis <dearhakeemdavis@gmail.com>
2018-03-22 01:37:30 -0400

    Add content for services section

---
+++ services.php
@@ -0,0 +1,33 @@
+<div class="container div-wrapper-margin-top">
+			<div id="div-main-body" class="row div-main-body">
+<div class="col-12 div-headline-left div-background-sub-headers">
+		<i class="fas fa-clipboard-list"></i> Our Services
+</div>
+<div class="col-12 div-headline-left div-background">
+	<p>
+		<span class="larg-first-letter">W</span>e video everything, from kids sport to corporate functions, covering stories locally, nationally and internacionally. Full service video production specialize in:
+	</p>
+	<div class="div-flex">
+		<div class="div-flex-one">
+			<ul class="ul-services">
+				<li>Interviews</li>
+				<li>Infomercials</li>
+				<li>Documentaries</li>
+				<li>Special events</li>
+				<li>Corporate Promotional</li>
+			</ul>
+		</div>
+		<div class="div-flex-one">
+			<ul class="ul-services">
+				<li>Short films</li>
+				<li>Weddings</li>
+				<li>Training videos</li>
+				<li>Sales and Marketing Videos</li>
+				<li>Family Parties and Quinces</li>
+			</ul>
+		</div>
+	</div>
+	<p>
+		Contact us to cover your event, increase brand awareness, reach your marketing objectives or just to record those specials moments to be remembered.
+	</p>
+</div>
