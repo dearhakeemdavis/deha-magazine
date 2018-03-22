ad18981713e22125f9c98e1d5d9b5cb3fad1c1d7
dearhakeemdavis <dearhakeemdavis@gmail.com>
2018-03-22 01:33:24 -0400

    Add section for top navigation

---
+++ nav.php
@@ -0,0 +1,35 @@
+<div class="col-6 div-mobile-logo">
+	<a href="index.php?page=home"><img class="img-logo display-inline padding-left" src="img/deha-logo.png" alt="Logo" title="Logo"/></a>
+</div>
+<div class="col-6 div-mobile-hamburger">
+	<img src="img/hamburger.png" alt="Mobile Navigation Icon" title="Mobile Navigation Icon" onClick="toggleMobileNav(isHidden)"/>
+</div>
+<div class="col-12" id="divmobilemenu">
+		<a href="index.php?page=home">Home</a><br>
+		<a href="index.php?page=about">About</a><br>
+		<a href="index.php?page=services">Services</a><br>
+		<a href="index.php?page=videos&category=entertainment">Videos</a><br>
+		<a href="index.php?page=news">News</a><br>
+		<a href="index.php?page=contact">Contact Us</a>
+</div>
+<div class="col-12 div-navi">
+	<div class="row">
+	<div class="col-12 col-sm-5 div-nav-link-container-left">
+		<div class="div-nav-link-wrap-left">
+		<a href="index.php?page=home" class="p-links display-inline">Home</a>
+		<a href="index.php?page=about" class="p-links display-inline padding-left">About</a>
+		<a href="index.php?page=services" class="p-links display-inline padding-left">Services</a>
+	</div>
+	</div>
+	<div class="col-12 col-sm-2 div-nav-logo-container">
+		<a href="index.php?page=home"><img class="img-logo display-inline padding-left" src="img/deha-logo.png" alt="Logo" title="Logo"/></a>
+	</div>
+	<div class="col-12 col-sm-5 div-nav-link-container-right">
+		<div class="div-nav-link-wrap-right">
+		<a href="index.php?page=videos&category=entertainment" class="p-links display-inline padding-left">Videos</a>
+		<a href="index.php?page=news" class="p-links display-inline padding-left">News</a>
+		<a href="index.php?page=contact" class="p-links display-inline padding-left">Contact Us</a>
+	</div>
+	</div>
+</div>
+</div>
