c4e11c6f70cc0bf68d4f0d314cf66f169e810277
dearhakeemdavis <dearhakeemdavis@gmail.com>
2018-03-22 01:24:49 -0400

    Add contact page

---
+++ contact.php
@@ -0,0 +1,19 @@
+<div class="container div-wrapper-margin-top">
+			<div id="div-main-body" class="row div-main-body">
+<div class="col-12 div-headline-left div-background-sub-headers">
+		<i class="far fa-comment-alt"></i> Contact Us
+</div>
+<div class="col-12 div-headline-left div-background">
+	<p>
+		<span class="larg-first-letter">W</span>e would love to hear from you.<br>Please fill out the form below to share your thoughts.
+	</p>
+	<form name="formGeneralContact" action="thankyou.php" method="post">
+	<label for="name">Name:</label><br>
+	<input type="text" name="name"/><br>
+	<label for="name">Email:</label><br>
+	<input type="text" name="email"/><br>
+	<label for="comment">Comment:</label><br>
+	<textarea cols="40" rows="8" name="comment"></textarea><br>
+	<button type="submit" name="submit" class="contact-submit">Submit</button>
+	</form>
+</div>
