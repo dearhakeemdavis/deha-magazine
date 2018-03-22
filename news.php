86f5fd0c09b17b11f75c11024167d3d190abc7d7
dearhakeemdavis <dearhakeemdavis@gmail.com>
2018-03-22 01:35:31 -0400

    Add news

---
+++ news.php
@@ -0,0 +1,10 @@
+<div class="container div-wrapper-margin-top">
+			<div id="div-main-body" class="row div-main-body">
+<div class="col-12 div-headline-left div-background-sub-headers">
+		<i class="far fa-newspaper"></i> News
+</div>
+<div class="col-12 div-headline-left div-background">
+	<p>
+		<span class="larg-first-letter">C</span>oming soon!
+	</p>
+</div>
